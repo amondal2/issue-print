cdb25f5bc09fee6438fb88c48d6b130595cad737
Agastya Mondal <amondal13@gmail.com>
2018-12-09 22:20:22 -0500

    add saving of access token method / command to parser

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
-console.log('test');
+const program = require('commander');
+const { prompt } = require('inquirer');
+const fs = require('fs');
 
+program.command('configure')
+  .action(async () => {
+    const questions = [{
+      type: 'Access token',
+      name: 'accesstoken',
+      message: 'Enter github access token',
+    }];
+    const answers = await prompt(questions);
+
+    await fs.writeFile('./config.txt', `${answers.accesstoken}\n`, (err) => {
+      if (err) { console.log(`Error writing access token. Failed with ${err}`); }
+      console.log('Access token saved. Run issue-print print to see assigned issues');
+    });
+  });
+
+
+program.arguments('print')
+  .action(() => console.log('ok'));
+
+program.parse(process.argv);
